4333c67100cb1494aacfddba316ed48c83e9ebf7
jikyubin <jikyubin@jikyubinui-MacBookPro.local>
2023-07-04 09:51:09 +0900

    fix: settings local 분리

--- FoodieZone/settings/__init__.py
+++ FoodieZone/settings/__init__.py
@@ -1,9 +1,11 @@
 from .base import *
 import os
 
-env = os.environ.get('ENV') if os.environ.get('ENV') else 'dev'
+env = os.environ.get('ENV') if os.environ.get('ENV') else 'local'
 
 if env == 'prod': 
     from .prod import *
-else:
+elif env == 'dev':
     from .dev import *
+else:
+    from .local import *
---
+++ FoodieZone/settings/prod.py
@@ -0,0 +1,15 @@
+from base import BASE_DIR
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432'
+    }
+}
