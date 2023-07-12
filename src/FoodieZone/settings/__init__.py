c6df75d9b488905ee347c7deac23bb6dd21a4994
jikyubin <jikyubin@jikyubinui-MacBookPro.local>
2023-07-12 17:58:36 +0900

    feat: Dockerfile initialize and path setting

---
+++ src/FoodieZone/settings/__init__.py
@@ -0,0 +1,11 @@
+from .base import *
+import os
+
+env = os.environ.get("ENV") if os.environ.get("ENV") else "local"
+
+if env == "prod":
+    from .prod import *
+elif env == "dev":
+    from .dev import *
+else:
+    from .local import *
---
+++ src/FoodieZone/settings/dev.py
@@ -0,0 +1,19 @@
+"""
+development 세팅
+"""
+
+from .base import BASE_DIR
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "5432",
+    }
+}
